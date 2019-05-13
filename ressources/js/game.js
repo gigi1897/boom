eeae33b5cd093ed6a2c2ea22dcc032d2d6af3156
Giuseppe D'Orsa <giuseppe.dorsa@hes-so.ch>
2019-05-13 22:31:30 +0200

    addedgeolocalisation

--- ressources/js/game.js
+++ ressources/js/game.js
@@ -350,6 +350,11 @@ function GameOver(){
 
             document.getElementById("explosedBloc1").innerHTML = hero.explosedBloc;
             document.getElementById("explosedBloc2").innerHTML = hero2.explosedBloc;
+
+            var script = document.createElement("script");  // create a script DOM node
+            script.src = "/ressources/js/location.js";  // set its src to the provided URL
+
+            document.head.appendChild(script);
         }
 };
 
